5ac58bf6d174bbeabf71f8f2e8454225b1307c9d
shamsu <Username@gmail.com>
2023-02-16 07:24:43 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A programe that prints a line using prinf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+pritf("with proper grammar, but the outcome is a piece of art,\n"
+return (0);
+}
+
+
+	
+
+
